751f5f8a06d14537f8ea20ddffd65aca5459d4ec
Zinkin_A/A <artemij.zinkovich@mail.ru>
2024-02-28 18:27:01 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './Mixins-and-Defolts/_defolt.scss';
+import 'normalize.css';
+import './App.scss';
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/NavBar/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="head">
+          <Navbar></Navbar>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/NavBar/navbar.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import logo from '../../Img/Logo.png'
+import './navbar.scss'
+
+const Navbar = () => {
+  return ( 
+    <div className="navbar container">
+      <img src={logo} alt="Logo" />
+      <nav>
+        <ul className="navbar-body">
+          <li><Link>Главная</Link></li>
+          <li><Link>Про гида</Link></li>
+          <li><Link>Программа тура</Link></li>
+          <li><Link>Стоимость</Link></li>
+          <li><Link>Блог</Link></li>
+          <li><Link>Контакты</Link></li>
+        </ul>
+      </nav>
+      <Link className="navbar-con-but">Консультация</Link>
+    </div>
+   );
+}
+ 
+export default Navbar
